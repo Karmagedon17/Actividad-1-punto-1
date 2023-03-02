6522141335bf7f1272779d589549453282371a5e
Karmagedon17 <117535110+Karmagedon17@users.noreply.github.com>
2023-03-02 14:07:41 -0500

    Add files via upload

---
+++ Ejercicio5.java
@@ -0,0 +1,15 @@
+package ejercicio5;
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        float SUMA,X,Y;
+        SUMA = 0;
+        X = 20;
+        SUMA = SUMA+X;
+        Y = 40;
+        X = X + Y*Y;
+        SUMA = SUMA + X/Y;
+        System.out.println("El valor de la suma es:"+SUMA);                           
+    }
+    
+}
